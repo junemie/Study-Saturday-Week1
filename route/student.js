126683c6c663f49db3173501aab7a38b50fdf8e0
June Hosoya <junehosoya@gmail.com>
2019-01-19 12:04:14 -0500

    cycle1

--- route/student.js
+++ route/student.js
@@ -1,11 +1,15 @@
-// let student = [ {id:1, name: 'Dan'}, {id:2, name: 'Karen' } ]
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+let student = [{ id: 1, name: "Dan" }, { id: 2, name: "Karen" }];
 
-
-
-
-
+//if someome wasnt to get information about sutdents, is to send that file for them to
+rounter.get("/", (req, res, next) => {
+  try {
+    res.send(students);
+  } catch (err) {
+    console.log(err);
+    //if we do have an error we want to make sure it encounters an error
+  }
+});
 
 module.exports = router;
