2f4362767ead68aa371a9f8486d50c8eddb034f6
Kristin Barr <kristinbarr@gmail.com>
2019-01-19 11:06:10 -0500

    first commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const routes = require('./route')
 
 // Init App
 const app = express();
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
+app.use(routes)
 // Listen on server
 
 app.listen(3000, () => {
